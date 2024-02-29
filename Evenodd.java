80d87347f8c6f5b15a0e04488bb331b2fbb034f3
Anjaligaddamidi <149222107+Anjaligaddamidi@users.noreply.github.com>
2024-02-29 13:04:05 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,11 @@
+class Evenodd
+{
+public static void main(String args[])
+{
+int n=Integer.parseInt(args[0]);
+if(n%2==0)
+System.out.println("number is"+n+"is even");
+else
+System.out.println("number is"+n+"is even");
+}
+}
